551c28a131f8341eecf37b697696eef164b7a8a9
vijisambath <168123980+vijisambath@users.noreply.github.com>
2024-04-26 00:10:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
