04166e861654f63349d9d1f95f0f6dfe6b187204
vijisambath <168123980+vijisambath@users.noreply.github.com>
2024-04-26 00:07:22 +0530

    Update streamlit_app.py
    connection change

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:'
     , my_dataframe
