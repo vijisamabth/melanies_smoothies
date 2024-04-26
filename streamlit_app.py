a4d5fec0c744244646270fba753ed9bf74a0b6f5
vijisambath <168123980+vijisambath@users.noreply.github.com>
2024-04-26 07:10:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write(
     """)
 
 cnx = st.connection("snowflake")
-session = cnx.session()
+session = cnx.sessio()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
